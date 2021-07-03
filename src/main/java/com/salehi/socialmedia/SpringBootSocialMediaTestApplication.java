5c3c774599b4ab18b4521033bc16cd553b5e0140
Arash <arashsalehi849@yahoo.com>
2021-07-03 13:39:10 +0430

    added index page and datasource/jpa properties

--- src/main/java/com/salehi/socialmedia/SpringBootSocialMediaTestApplication.java
+++ src/main/java/com/salehi/socialmedia/SpringBootSocialMediaTestApplication.java
@@ -2,12 +2,20 @@ package com.salehi.socialmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class SpringBootSocialMediaTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSocialMediaTestApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String goToIndex(){
+        return "index";
     }
 
 }
