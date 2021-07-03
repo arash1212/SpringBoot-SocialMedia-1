945620145fdbf079d141a22baf06d4a67df22a38
Arash <arashsalehi849@yahoo.com>
2021-07-03 12:29:09 +0430

    added project to git repository

---
+++ src/main/java/com/salehi/socialmedia/SpringBootSocialMediaTestApplication.java
@@ -0,0 +1,13 @@
+package com.salehi.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSocialMediaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSocialMediaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salehi/socialmedia/SpringBootSocialMediaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salehi.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSocialMediaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
