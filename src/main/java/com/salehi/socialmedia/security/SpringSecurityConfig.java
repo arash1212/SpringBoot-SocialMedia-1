49cac4789aa96a7961741b2355711bb6598ae5ee
Arash <arashsalehi849@yahoo.com>
2021-07-06 23:50:56 +0430

    added bootstrap to project

--- src/main/java/com/salehi/socialmedia/security/SpringSecurityConfig.java
+++ src/main/java/com/salehi/socialmedia/security/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,9 +27,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/", "/login","/signup","/user/signUp.do","/user/sendPost.do").permitAll()
+                .antMatchers("/", "/login", "/signup", "/user/signUp.do", "/user/sendPost.do","/css/**").permitAll()
                 .anyRequest().authenticated().and().formLogin().loginPage("/login").usernameParameter("username")
-        .passwordParameter("password");
+                .passwordParameter("password");
     }
 
     @Override
@@ -37,8 +38,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
